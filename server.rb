38c220fe39e78612d8ef717c3603cfef5ab4162d
John Doe <christopheraarguello@gmail.com>
2018-02-14 15:53:17 -0500

    added kims count

--- server.rb
+++ server.rb
@@ -12,22 +12,22 @@ get '/' do
   send_file File.join(settings.public_folder, 'new_home.html')
 end
 
-get '/nometa' do
-  send_file File.join(settings.public_folder, 'noMetaMask.html')
-end
+# get '/nometa' do
+#   send_file File.join(settings.public_folder, 'noMetaMask.html')
+# end
 
-get '/kimsForSale' do
-  send_file File.join(settings.public_folder, 'kimsForSale.html')
-end
+# get '/kimsForSale' do
+#   send_file File.join(settings.public_folder, 'kimsForSale.html')
+# end
 
-get '/search' do
-  send_file File.join(settings.public_folder, 'searchForKim.html')
-end
+# get '/search' do
+#   send_file File.join(settings.public_folder, 'searchForKim.html')
+# end
 
 get '/myKims' do
   send_file File.join(settings.public_folder, 'mykims.html')
 end
 
-get '/test' do
-  send_file File.join(settings.public_folder, 'test.html')
-end
+# get '/test' do
+#   send_file File.join(settings.public_folder, 'test.html')
+# end
